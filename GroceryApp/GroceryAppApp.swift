fc35ec9d8282ff48c78422ccbe73691f4494f192
maheshprasad <mahesh223prasad@gmail.com>
2022-05-03 02:03:28 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Mahesh Prasad on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
